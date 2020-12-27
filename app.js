24783ae26c1f55530b8a7c1a2897640a8208d221
Sharan <karthikeyansharan00@gmail.com>
2020-12-27 20:47:20 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,64 @@
+const { argv } = require("yargs");
+const yargs = require("yargs");
+const chalk = require("chalk");
+const database = require("./database");
+
+yargs.command({
+  command: "create",
+  describe:
+    "Create a data.\nExample:" +
+    chalk.green(' node app.js create --mail="your mail id" --data="your data"'),
+  builder: {
+    mail: {
+      describe: chalk.yellow(
+        "User Mail ID. " + chalk.red.bgYellow.bold("(This is your KEY)")
+      ),
+      demandOption: true,
+      type: String,
+    },
+    data: {
+      describe: chalk.yellow("User Data"),
+      demandOption: true,
+      type: String,
+    },
+  },
+  handler: function (argv) {
+    database.createData(argv.mail, argv.data);
+  },
+});
+
+yargs.command({
+  command: "read",
+  describe:
+    "Read a data\nExample: " +
+    chalk.green('node app.js read --mail="your mail id"'),
+  builder: {
+    mail: {
+      describe: chalk.yellow("Read data by using mail ID"),
+      demandOption: true,
+      type: String,
+    },
+  },
+  handler: function (argv) {
+    database.readData(argv.mail);
+  },
+});
+
+yargs.command({
+  command: "delete",
+  describe:
+    "Delete a data\nExample: " +
+    chalk.green('node app.js delete --mail="your mail id"'),
+  builder: {
+    mail: {
+      describe: chalk.yellow("Delete Data by using mail ID"),
+      demandOption: true,
+      type: String,
+    },
+  },
+  handler: function (argv) {
+    database.deleteData(argv.mail);
+  },
+});
+
+yargs.parse();
---
+++ database.js
@@ -0,0 +1,104 @@
+const fs = require("fs");
+const chalk = require("chalk");
+
+const createData = (mail, data) => {
+  const database = loadDatabase();
+
+  const duplicateData = database.filter((data) => {
+    return data.mail === mail;
+  });
+
+  if (duplicateData.length === 0) {
+    database.push({
+      mail: mail,
+      data: data,
+    });
+    saveDatabase(database);
+    console.log(
+      chalk.green("Success Data Added... :)\n\n") +
+        chalk.yellow(
+          "Check " +
+            chalk.red("database.json") +
+            " file in the current project directory."
+        )
+    );
+  } else {
+    console.log(
+      chalk.red("Sorry, This Mail ID have a data :(\n\n") +
+        chalk.yellow(
+          "Check " +
+            chalk.red("database.json") +
+            " file in the current project directory."
+        )
+    );
+  }
+};
+
+const readData = (mail) => {
+  const database = loadDatabase();
+  const data = database.find((data) => {
+    return data.mail === mail;
+  });
+
+  if (data) {
+    console.log(chalk.green("Hola, We retrived a data as JSON :)\n"));
+    console.log(data);
+  } else {
+    console.log(
+      chalk.red("Data not found... :(\n\n") +
+        chalk.yellow(
+          "Check " +
+            chalk.red("database.json") +
+            " file in the current project directory."
+        )
+    );
+  }
+};
+
+const deleteData = (mail) => {
+  const database = loadDatabase();
+  const dataToKeep = database.filter((data) => {
+    return data.mail !== mail;
+  });
+  saveDatabase(dataToKeep);
+  if (database.length > dataToKeep.length) {
+    console.log(
+      chalk.green("User Data Deleted successfully... :)\n\n") +
+        chalk.yellow(
+          "Check " +
+            chalk.red("database.json") +
+            " file in the current project directory."
+        )
+    );
+  } else {
+    console.log(
+      chalk.red("No data found... :(\n\n") +
+        chalk.yellow(
+          "Check " +
+            chalk.red("database.json") +
+            " file in the current project directory."
+        )
+    );
+  }
+};
+
+const loadDatabase = () => {
+  try {
+    const dataBuffer = fs.readFileSync("database.json");
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (err) {
+    return [];
+  }
+};
+
+const saveDatabase = (database) => {
+  const dataJSON = JSON.stringify(database);
+  fs.writeFileSync("database.json", dataJSON);
+};
+
+module.exports = {
+  createData: createData,
+  readData: readData,
+  deleteData: deleteData,
+};
